a221d85863d83a6b8182c59cc8318fd31264ea59
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-05-14 17:36:55 +0200

    Add project files.

---
+++ SiyayaTravelAssist/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiyayaTravelAssist.Startup))]
+namespace SiyayaTravelAssist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
