746e749918b855150cfc0854ad94aaa31442a5ac
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-05-14 19:47:38 +0200

    Define Service Interfaces

---
+++ SiyayaTravelAssist/Core/Services/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SiyayaTravelAssist.Core.Domain;
+
+namespace SiyayaTravelAssist.Core.Services
+{
+    public interface IBookingService
+    {
+        //The implementations of the ListAsync method must asynchronously return an enumeration of Booking.
+        Task<IEnumerable<Booking>> ListAsync();
+    }
+}
---
+++ SiyayaTravelAssist/Core/Services/ITripService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SiyayaTravelAssist.Core.Domain;
+
+namespace SiyayaTravelAssist.Core.Services
+{
+    public interface ITripService
+    {
+        Task<IEnumerable<Trip>> ListAsync();
+    }
+}
