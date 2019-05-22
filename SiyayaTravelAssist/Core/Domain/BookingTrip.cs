e0e4102b3374d5c00b42f3eac62fba1a9ce3ece8
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-05-22 17:12:54 +0200

    Sprint 2: Configure BookingTrip Entity

--- SiyayaTravelAssist/Core/Domain/BookingTrip.cs
+++ SiyayaTravelAssist/Core/Domain/BookingTrip.cs
@@ -4,5 +4,19 @@
     {
         public int Id { get; set; }
 
+        //One Booking - Many BookingTrips (BookingTrip is like H)
+        public virtual Booking Booking { get; set; }
+        public int BookingId { get; set; }
+
+        //BookingTrip - Driver (Entity is Like H)
+        public virtual Driver Driver { get; set; }
+        public int DriverId { get; set; }
+
+        //BookingTrip - Vehicle (Entity is Like H)
+        public virtual Vehicle Vehicle { get; set; }
+        public int VehicleId { get; set; }
+
+        //BookingTrip - Trip (Entity is Like B)
+        public Trip Trip { get; set; }
     }
 }
