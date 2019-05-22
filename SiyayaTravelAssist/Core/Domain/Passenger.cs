9d3f8751cf5ae3beae9859f44bfe99d57aee63c7
kgothatsotlaka <mankgwanyanetlaka@gmail.com>
2019-05-22 18:50:04 +0200

    Sprint 2: Configure Passenger Entity

--- SiyayaTravelAssist/Core/Domain/Passenger.cs
+++ SiyayaTravelAssist/Core/Domain/Passenger.cs
@@ -1,14 +1,22 @@
-﻿namespace SiyayaTravelAssist.Core.Domain
+﻿using System.Collections.Generic;
+
+namespace SiyayaTravelAssist.Core.Domain
 {
     public class Passenger
     {
+        public Passenger()
+        {
+            Trips = new HashSet<Trip>();
+        }
         public int Id { get; set; }
         public string FullName { get; set; }
         public string LastName { get; set; }
-        public string Address { get; set; }
         public int Telephone { get; set; }
 
-        public Trip TripId { get; set; }
+        //Trip - Passenger (M - 1) (C- H)
+        public ICollection<Trip> Trips { get; set; }
+
+
 
     }
 }
